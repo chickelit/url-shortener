0931ec5deb9cbdd58c9d3fa50b55664d2e7c1904
jose13162 <anselmojose297@gmail.com>
2022-09-03 16:05:21 -0300

    Add a validation for the shortened url creating route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import storeValidator from "../validators/shortenedUrl/storeValidator";
 
 const router = Router();
 
@@ -7,7 +8,19 @@ router.get("/", (request: Request, response: Response) => {
 });
 
 router.post("/", (request: Request, response: Response) => {
-  return response.send(JSON.stringify(request.body));
+  try {
+    const { body } = request;
+    const { error } = storeValidator.validate(body);
+
+    if (error) {
+      return response.status(422).json(error);
+    }
+
+    // TODO: create the shortened url
+    return response.status(200).json("OK");
+  } catch (error) {
+    return response.status(500).json(error);
+  }
 });
 
 export default router;
---
+++ src/validators/shortenedUrl/storeValidator.ts
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+const schema = Joi.object({
+  url: Joi.string().uri().required(),
+});
+
+export default schema;
