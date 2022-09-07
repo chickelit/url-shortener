adc7695709b62d114868aa5118795950d4658603
jose13162 <anselmojose297@gmail.com>
2022-09-07 11:59:06 -0300

    Configure bodyparser

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { ShortenedUrl } from "../database/sequelize";
 import { Request, Response, Router } from "express";
 import storeValidator from "../validators/shortenedUrl/storeValidator";
 import crypto from "crypto";
+import bodyParser from "body-parser";
 
 const router = Router();
 
@@ -10,29 +11,33 @@ interface PostAttributes {
   redirectUrl: string;
 }
 
-router.post("/shortened-urls", async (request: Request, response: Response) => {
-  try {
-    const body: PostAttributes = request.body;
-    const { error } = storeValidator.validate(body);
+router.post(
+  "/shortened-urls",
+  bodyParser.json(),
+  async (request: Request, response: Response) => {
+    try {
+      const body: PostAttributes = request.body;
+      const { error } = storeValidator.validate(body);
 
-    if (error) {
-      return response.status(422).json(error);
+      if (error) {
+        return response.status(422).json(error);
+      }
+
+      const key = crypto.randomBytes(4).toString("hex");
+      const redirectUrl = body.redirectUrl.replace(/\/$/, "");
+
+      const shortenedUrl = await ShortenedUrl.create({
+        key,
+        originalUrl: body.url,
+        shortenedUrl: `${redirectUrl}/${key}`,
+      });
+
+      return response.status(200).json(shortenedUrl);
+    } catch (error) {
+      return response.status(500).json(error);
     }
-
-    const key = crypto.randomBytes(4).toString("hex");
-    const redirectUrl = body.redirectUrl.replace(/\/$/, "");
-
-    const shortenedUrl = await ShortenedUrl.create({
-      key,
-      originalUrl: body.url,
-      shortenedUrl: `${redirectUrl}/${key}`,
-    });
-
-    return response.status(200).json(shortenedUrl);
-  } catch (error) {
-    return response.status(500).json(error);
   }
-});
+);
 
 router.get("/:key", async (request: Request, response: Response) => {
   const { key } = request.params;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,19 @@ dotenv.config();
 
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import routes from "./routes";
 import { sequelize } from "./database/sequelize";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(routes);
 
